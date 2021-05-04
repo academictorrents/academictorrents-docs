72441301a3137e7d0443eb3330cc676a3781af72
Joseph Paul Cohen <joseph@josephpcohen.com>
2021-05-04 14:56:19 -0400

    local compile

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Academic Torrents Documentation'
-copyright = u'2020, Academic Torrents'
+copyright = u'2021, Academic Torrents'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
