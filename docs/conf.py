9f73ea2732228d628b044a4a64dddc5d2351637a
Joseph Paul Cohen <joseph@josephpcohen.com>
2021-05-04 15:00:49 -0400

    theme config

--- docs/conf.py
+++ docs/conf.py
@@ -256,3 +256,22 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+
+html_theme_options = {
+    'analytics_id': 'UA-46214407-1',
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
