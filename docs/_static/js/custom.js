eab55b58eb1c1884b45abbf12a96ecee2f6d78a5
Joseph Paul Cohen <joseph@josephpcohen.com>
2025-08-08 08:59:55 +0000

    js

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,3 +1,8 @@
+
+var ascript = document.createElement('script');
+ascript.setAttribute('src','https://media.ethicalads.io/media/client/ethicalads.min.js');
+document.head.appendChild(ascript);
+
 $.ajax("/a_router.php?color=dark", {
 	success: function( data ) {
 	    $(".wy-side-scroll").append(data);
